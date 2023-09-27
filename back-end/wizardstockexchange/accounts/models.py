650466f692c941218716eef92a55cbe8ff31bf78
GearHead87 <hosanulislam87@gmail.com>
2023-09-28 02:22:48 +0600

    fix superuser bug

--- back-end/wizardstockexchange/accounts/models.py
+++ back-end/wizardstockexchange/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
     
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, username, password=None,  *args, **kwargs):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
@@ -13,7 +13,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password=None):
+    def create_superuser(self, email, username, password=None, *args, **kwargs):
         user = self.create_user(email, username, password)
         user.is_staff = True
         user.is_superuser = True
@@ -24,11 +24,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=30, unique=True)
     full_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    phone_number = models.IntegerField()
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=30)
-    country = models.CharField(max_length=30)
-    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=30, null=True)
+    country = models.CharField(max_length=30, null=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
@@ -37,7 +37,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username','full_name', 'phone_number','address','city','country', 'date_of_birth']
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.email
