6039fbb1f066e3859859253c055025ac5ad50f58
Parvez Islam <parvezislam45@gmail.com>
2023-10-11 18:25:08 +0600

    name change

--- front-end/wizzard-stock-exchange/src/Pages/Home/Ratio.jsx
+++ front-end/wizzard-stock-exchange/src/Pages/Home/Ratio.jsx
@@ -35,7 +35,7 @@ const Ratio = () => {
                   type="button"
                   className="px-4 py-2 text-sm font-medium text-gray-900 bg-pink-500 border border-gray-900 rounded-r-md hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
                 >
-                  JISAN
+                  Meharab
                 </button>
                 <button
                   type="button"
