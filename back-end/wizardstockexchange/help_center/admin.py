089812c6c3538b179104ef7d05054fa24c165247
MD. Mehrabul Islam Zeshan <102683822+MehrabZishaan@users.noreply.github.com>
2023-10-12 20:39:00 +0600

    serializers.py added

--- back-end/wizardstockexchange/help_center/admin.py
+++ back-end/wizardstockexchange/help_center/admin.py
@@ -3,4 +3,11 @@ from .models import ContactMessage
 
 # Register your models here.
 
-admin.site.register(ContactMessage)
+# admin.site.register(ContactMessage)
+
+# Register the ContactMessage model with the admin
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'subject']
+    search_fields = ['name', 'email', 'subject']
+    list_filter = ['subject']
--- back-end/wizardstockexchange/help_center/urls.py
+++ back-end/wizardstockexchange/help_center/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import help_center
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ContactMessageViewSet, help_center
+
+# Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'contact-messages', ContactMessageViewSet)
 
 urlpatterns = [
     path('', help_center, name='help_center'),
+    path('api/', include(router.urls)),  # Include the API URLs
 ]
