a26d7d5694c4a564a8ef9233df403bd1952510d7
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2023-10-17 13:00:08 +0600

    updated

---
+++ front-end/wizzard-stock-exchange/src/Wallet/Wallet.jsx
@@ -0,0 +1,270 @@
+const Wallet = () => {
+  return (
+    <div>
+      <main className="relative z-0 flex-1 pb-8 px-6 bg-white mt-20">
+        <div className="grid pb-10  mt-4 ">
+          <div className="mb-2">
+            <p className="text-lg font-semibold text-gray-400">Invoices</p>
+          </div>
+          <div className="grid grid-cols-12 gap-6 border-b-2 pb-5">
+            <div className="col-span-12 sm:col-span-12 md:col-span-8 lg:col-span-8 xxl:col-span-8">
+              <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 mt-3">
+                <div className="p-4">
+                  <p className="text-xl font-bold">RM 45,941</p>
+                  <p className="text-xs font-semibold text-gray-400">Overdue</p>
+                </div>
+                <div className="p-4">
+                  <p className="text-xl font-bold">RM 37,500</p>
+                  <p className="text-xs font-semibold text-gray-400">
+                    Total Outstanding
+                  </p>
+                </div>
+                <div className="p-4">
+                  <p className="text-xl font-bold">RM 9,200</p>
+                  <p className="text-xs font-semibold text-gray-400">
+                    In Process
+                  </p>
+                </div>
+                <div className=" p-4">
+                  <p className="text-xl font-bold">RM 5,700</p>
+                  <p className="text-xs font-semibold text-gray-400">
+                    Paid Today
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 xxl:col-span-4">
+              <div className="p-4">
+                <p className="text-sm text-gray-400">Outstanding Revenue</p>
+                <div className="shadow w-full bg-gray-100 mt-2">
+                  <div
+                    className="bg-indigo-600 text-xs leading-none py-1 text-center text-white"
+                    
+                  ></div>
+                </div>
+                <p className="text-xs font-semibold text-gray-400 mt-2">
+                  RM 45,941 Overdue
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-2 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 mt-3">
+            <div
+              className="relative w-full h-52 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out"
+              style={{ backgroundImage: "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f868ecef-4b4a-4ddf-8239-83b2568b3a6b/de7hhu3-3eae646a-9b2e-4e42-84a4-532bff43f397.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg8OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y4NjhlY2VmLTRiNGEtNGRkZi04MjM5LTgzYjI1NjhiM2E2YlwvZGU3aGh1My0zZWFlNjQ2YS05YjJlLTRlNDItODRhNC01MzJiZmY0M2YzOTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R0h-BS0osJSrsb1iws4-KE43bUXHMFvu5PvNfoaoi8o')" }}
+            >
+              <div className="absolute inset-0 bg-pink-900 bg-opacity-75 transition duration-300 ease-in-out"></div>
+              <div className="relative w-full h-full px-4 sm:px-6 lg:px-4 flex items-center justify-center">
+                <div>
+                  <h3 className="text-center text-white text-lg">
+                    Total Balance
+                  </h3>
+                  <h3 className="text-center text-white text-3xl mt-2 font-bold">
+                    RM 27,580
+                  </h3>
+                  <div className="flex space-x-4 mt-4">
+                    <button
+                      className="block uppercase mx-auto shadow bg-white text-indigo-600 focus:shadow-outline 
+                                  focus:outline-none text-xs py-3 px-4 rounded font-bold"
+                    >
+                      Transfer
+                    </button>
+                    <button
+                      className="block uppercase mx-auto shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline 
+                                   focus:outline-none text-white text-xs py-3 px-4 rounded font-bold"
+                    >
+                      Request
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              className="relative w-full h-52 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out"
+              style={{ backgroundImage: "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f868ecef-4b4a-4ddf-8239-83b2568b3a6b/de7hhu3-3eae646a-9b2e-4e42-84a4-532bff43f397.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y4NjhlY2VmLTRiNGEtNGRkZi04MjM5LTgzYjI1NjhiM2E2YlwvZGU3aGh1My0zZWFlNjQ2YS05YjJlLTRlNDItODRhNC01MzJiZmY0M2YzOTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R0h-BS0osJSrsb1iws4-KE43bUXHMFvu5PvNfoaoi8o')" }}
+            >
+              <div className="absolute inset-0 bg-yellow-600 bg-opacity-75 transition duration-300 ease-in-out"></div>
+              <div className="relative w-full h-full px-4 sm:px-6 lg:px-4 flex items-center">
+                <div>
+                  <div className="text-white text-lg flex space-x-2 items-center">
+                    <div className="bg-white rounded-md p-2 flex items-center">
+                      <i className="fas fa-toggle-off fa-sm text-yellow-300"></i>
+                    </div>
+                    <p>Finished Appt</p>
+                  </div>
+                  <h3 className="text-white text-3xl mt-2 font-bold">120</h3>
+                  <h3 className=" text-lg mt-2 text-yellow-100 ">
+                    4 not confirmed
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div
+              className="relative w-full h-52 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out"
+              style={{ backgroundImage: "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f868ecef-4b4a-4ddf-8239-83b2568b3a6b/de7hhu3-3eae646a-9b2e-4e42-84a4-532bff43f397.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y4NjhlY2VmLTRiNGEtNGRkZi04MjM5LTgzYjI1NjhiM2E2YlwvZGU3aGh1My0zZWFlNjQ2YS05YjJlLTRlNDItODRhNC01MzJiZmY0M2YzOTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R0h-BS0osJSrsb1iws4-KE43bUXHMFvu5PvNfoaoi8o')" }}
+            >
+              <div className="absolute inset-0 bg-blue-900 bg-opacity-75 transition duration-300 ease-in-out"></div>
+              <div className="relative w-full h-full px-4 sm:px-6 lg:px-4 flex items-center">
+                <div>
+                  <div className="text-white text-lg flex space-x-2 items-center">
+                    <div className="bg-white rounded-md p-2 flex items-center">
+                      <i className="fas fa-clipboard-check fa-sm text-blue-800"></i>
+                    </div>
+                    <p>Finished Appt</p>
+                  </div>
+                  <h3 className="text-white text-3xl mt-2 font-bold">72</h3>
+                  <h3 className="text-white text-lg mt-2 ">
+                    3.4%{" "}
+                    <span className="font-semibold text-blue-200">
+                      vs last month
+                    </span>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <div className="bg-white shadow rounded-lg mb-4 p-4 sm:p-6 h-full w-2/4 mx-auto">
+                  <div className="flex items-center justify-between mb-6">
+                    <div className="flex justify-center items-center gap-6">
+                      <a href="">
+                        <h3 className="text-sm font-semibold leading-none text-gray-900">
+                          Open Docker(2)
+                        </h3>
+                      </a>
+                      <a href="">
+                        <h3 className="text-sm font-semibold leading-none text-gray-900">
+                          Position (1)
+                        </h3>
+                      </a>
+                    </div>
+
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-cyan-600 hover:bg-gray-100 rounded-lg inline-flex items-center p-2"
+                    >
+                      <img
+                        className="w-5 h-6"
+                        src="https://static.vecteezy.com/system/resources/thumbnails/026/753/173/small/save-icon-icon-for-your-website-mobile-presentation-and-logo-design-vector.jpg"
+                        alt=""
+                      />
+                    </a>
+                  </div>
+                  <div className="flex items-center justify-between mb-4">
+                    <div className="flex items-center">
+                      <img
+                        className="mr-2 w-5 h-5 rounded-full"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjMiX1PusL0DZjwvQCE5nmT8XDvRZhVqDDLjMJ9EG7YpoFwG62VzSqprkuu7ydokIJdwc&usqp=CAU"
+                        alt=""
+                      />
+
+                      <p>
+                        <span className="text-md font-semibold">
+                          Hide Other Pairs
+                        </span>
+                      </p>
+                    </div>
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-cyan-600 hover:bg-gray-100 rounded-lg inline-flex items-center p-2"
+                    >
+                      Clear All
+                    </a>
+                  </div>
+                  <div className="flex items-center justify-between mb-4">
+                    <div className="flex items-center">
+                      <img
+                        className="mr-2 w-7 h-7 rounded-full"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjMiX1PusL0DZjwvQCE5nmT8XDvRZhVqDDLjMJ9EG7YpoFwG62VzSqprkuu7ydokIJdwc&usqp=CAU"
+                        alt=""
+                      />
+
+                      <p>
+                        <span className="text-lg font-bold">
+                          {" "}
+                          BIDKUT{" "}
+                          <span className="text-md font-semibold">
+                            Prpitual
+                          </span>
+                        </span>
+                      </p>
+                    </div>
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-cyan-600 hover:bg-gray-100 rounded-lg inline-flex items-center p-2"
+                    >
+                      <img
+                        className="w-5"
+                        src="https://cdn.icon-icons.com/icons2/2645/PNG/512/box_arrow_in_up_right_icon_160373.png"
+                        alt=""
+                      />
+                    </a>
+                  </div>
+                  <div className="flex justify-between items-center mb-3">
+                    <h6 className="text-xl font-normal">Cross 5X !!! ?</h6>
+                    <div className="flex items-center ">
+                      <img
+                        className="mr-2 w-5 h-5 rounded-full"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjMiX1PusL0DZjwvQCE5nmT8XDvRZhVqDDLjMJ9EG7YpoFwG62VzSqprkuu7ydokIJdwc&usqp=CAU"
+                        alt=""
+                      />
+
+                      <p>
+                        <span className="text-md font-semibold">
+                          Risk 1.74 %
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex justify-between items-center mb-3">
+                    <h1 className="text-md font-semibold">Size (BTC)</h1>
+                    <h1 className="text-md font-semibold">
+                      UnRealize PNL(%RDC)
+                    </h1>
+                    <h1 className="text-md font-semibold">Margin</h1>
+                  </div>
+                  <div className="flex justify-between items-center mb-3">
+                    <h1 className="text-md font-semibold">0.0007</h1>
+                    <h1 className="text-md font-semibold text-red-600">
+                      -0.01[-0.15%]
+                    </h1>
+                    <h1 className="text-md font-semibold">6.81</h1>
+                  </div>
+                  <div className="flex justify-between items-center mb-3">
+                    <h1 className="text-md font-semibold">Entry Price</h1>
+                    <h1 className="text-md font-semibold">Mark Price</h1>
+                    <h1 className="text-md font-semibold">Licudation Price</h1>
+                  </div>
+                  <div className="flex justify-between items-center mb-3">
+                    <h1 className="text-md font-semibold">340.4953</h1>
+                    <h1 className="text-md font-semibold">367.6590</h1>
+                    <h1 className="text-md font-semibold">426.8752</h1>
+                  </div>
+                  <h1 className="mb-8">TP/SL -- / --</h1>
+                  <div className="flex justify-between items-center mb-3">
+                    <button
+                      type="button"
+                      className="py-2.5 px-5 text-xs font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >
+                      Adjust Avarage
+                    </button>
+                    <button
+                      type="button"
+                      className="py-2.5 px-5  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >
+                      Stop Profit & Loss
+                    </button>
+                    <button
+                      type="button"
+                      className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    >
+                      Close Position
+                    </button>
+                  </div>
+                </div>
+    </div>
+  );
+};
+
+export default Wallet;
