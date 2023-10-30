185468dd813ab32da332f17040a0f31f8cf8a478
Parvez Islam <parvezislam45@gmail.com>
2023-10-30 10:34:29 +0600

    chat

--- front-end/wizzard-stock-exchange/Payment/CheckoutForm.jsx
+++ front-end/wizzard-stock-exchange/Payment/CheckoutForm.jsx
@@ -28,8 +28,8 @@ const CheckoutForm = () => {
     }
   };
   return (
-    <div className="bg-yellow-600 text-white h-52 w-96 p-6 rounded-xl shadow-md mx-auto mt-20">
-      <form className="w-2/4" onSubmit={handleSubmit}>
+    <div className="bg-yellow-600 text-white h-40 w-3/4 p-6  mx-auto mt-">
+      <form className="w-full px-10" onSubmit={handleSubmit}>
         <CardElement
           options={{
             style: {
@@ -47,11 +47,7 @@ const CheckoutForm = () => {
           }}
         />
         <div className="text-center">
-        <button type="submit"
-          disabled={!stripe} className="text-white bg-black hover:bg-red-600 mt-10 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
-  Check out
-  <svg className="w-5 h-5 ml-2 -mr-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg>
-</button>
+        
         </div>
         
       </form>
--- front-end/wizzard-stock-exchange/src/layout/Main.jsx
+++ front-end/wizzard-stock-exchange/src/layout/Main.jsx
@@ -1,16 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../Shered/Nav";
 import Footer from "../Shered/Footer";
-
+import ChatModal from "../CustomerSuport/ChatModal";
 
 const Main = () => {
-    return (
+  return (
+    <div className="">
+      <Nav />
+      <Outlet />
+      <div className="flex items-end justify-end fixed bottom-0 right-0 mb-20 mr-4 z-10">
         <div>
-            <Nav/>
-            <Outlet/>
-            <Footer/>
+          
+          <ChatModal/>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
